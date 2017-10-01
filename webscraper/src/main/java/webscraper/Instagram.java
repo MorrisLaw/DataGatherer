f5cd06cfe550a38ef834b7dbe01e3ccc3a161c29
Jeremy L. Morris <jeremylevanmorris@gmail.com>
2017-10-01 00:12:08 -0400

    Added abstract class scraper. And fixed dependency issues with selenium.

---
+++ webscraper/src/main/java/webscraper/Instagram.java
@@ -0,0 +1,17 @@
+package webscraper;
+
+import java.util.ArrayList;
+
+public class Instagram extends Scraper {
+
+	@Override
+	ArrayList<String> getChildUrls() {
+		return null;
+	}
+
+	@Override
+	ArrayList<String> getHashTags() {
+		return null;
+	}
+	
+}
